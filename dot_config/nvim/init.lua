7da59751b4d2e76aa05fc455b0890cf581e368ad
Leftheris Amperiadis <lefty.amperiadis@proton.me>
2025-05-02 19:42:17 -0500

    Update init.lua to add codecompanion, terminal and quickfix changes

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -39,7 +39,27 @@ require('lazy').setup({
   {'nvim-telescope/telescope-live-grep-args.nvim'},
   {'nvim-telescope/telescope-file-browser.nvim'},
   {'tpope/vim-fugitive'},
-  {'otavioschwanck/arrow.nvim', dependencies = { 'echasnovski/mini.icons' }},
+  {'nvim-treesitter/nvim-treesitter'},
+  {'zbirenbaum/copilot.lua'},
+  {'akinsho/toggleterm.nvim', version = "*", config = true},
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim", -- Make Markdown buffers look beautiful
+    ft = { "markdown", "codecompanion" },
+    opts = {
+      render_modes = true,
+      sign = {
+        enabled = false, -- Turn off in the status column
+      },
+    },
+  },
 })
 
 -- LSP Section --
@@ -78,6 +98,7 @@ require('mason-lspconfig').setup({
 local cmp = require('cmp')
 cmp.setup({
   sources = {
+    {name = 'codecompanion'},
     {name = 'nvim_lsp'},
   },
   mapping = cmp.mapping.preset.insert({
@@ -101,24 +122,88 @@ cmp.setup({
 
  -- TELESCOPE SECTION -- 
 local builtin = require('telescope.builtin')
+require('telescope').setup({
+  defaults = {
+    cache_picker = {
+      num_pickers = 10,
+      ignore_empty_prompt = 'true'
+    }
+  }
+})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set("n", "<leader>fc", require("telescope-live-grep-args.shortcuts").grep_word_under_cursor)
 vim.keymap.set("n", "<leader>fd", ":Telescope file_browser<CR>")
 vim.keymap.set("n", "<leader>fs", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+vim.keymap.set('n', '<leader>fp', builtin.pickers, { desc = 'Telesope display all used pickers' })
+vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = 'Telesope display search history' })
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('live_grep_args')
 require("telescope").load_extension('file_browser')
 
--- ARROW SECTION --
-require('arrow').setup({
-  show_icons = true,
-  leader_key = ';', -- Recommended to be a single key
-  buffer_leader_key = 'm', -- Per Buffer Mappings
+-- COPILOT SECTION --
+require('copilot').setup({
+  suggestion = { enabled = false },
+  panel = { enabled = false },
 })
+
+require("codecompanion").setup({
+  strategies = {
+    chat = {
+      adapter = "copilot",
+    },
+    inline = {
+      adapter = "copilot",
+    },
+  },
+})
+
+require('render-markdown').setup()
+
+vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>fa", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('x', 'y', '"+y', { noremap = true, silent = true } )
+
+-- TERMINAL SECTION --
+require("toggleterm").setup({
+  start_in_insert = true,       -- Automatically enter terminal mode
+  persist_mode = false          -- Don't remember last mode (forces insert)
+})
+vim.keymap.set('t', '<esc>', '<C-\\><C-N>', t_opts)
+vim.keymap.set({ "n", "v", "t" }, "<C-t>", "<cmd>ToggleTerm direction=float<CR>")
+
+vim.keymap.set({ "n", "v" }, "<C-h>", "<C-w>h")
+vim.keymap.set({ "n", "v" }, "<C-j>", "<C-w>j")
+vim.keymap.set({ "n", "v" }, "<C-k>", "<C-w>k")
+vim.keymap.set({ "n", "v" }, "<C-l>", "<C-w>l")
+
+vim.g.clipboard = 'osc52'
 
 vim.o.termguicolors = true
 vim.cmd.colorscheme('gruvbox')
 vim.o.background = "dark"
+
+
+-- QUICKFIX SECTION --
+vim.api.nvim_create_user_command("BuildQuickfix", function()
+  local log_path = vim.fn.expand("~/source/build_logs/build.log")
+  if vim.fn.filereadable(log_path) == 0 then
+    vim.notify("No build.log found at " .. log_path, vim.log.levels.WARN)
+    return
+  end
+
+  vim.cmd("cgetfile " .. log_path)
+  vim.cmd("copen")
+end, {})
+vim.opt.errorformat = "%f:%l:%c: error: %m"
+
+-- Quickfix navigation with custom leader keys
+vim.keymap.set("n", "<leader>ch", ":cnext<CR>", { desc = "Next quickfix item" })
+vim.keymap.set("n", "<leader>cl", ":cprev<CR>", { desc = "Previous quickfix item" })
+vim.keymap.set("n", "<leader>co", ":copen<CR>", { desc = "Open quickfix list" })
+vim.keymap.set("n", "<leader>cc", ":cclose<CR>", { desc = "Close quickfix list" })
+vim.keymap.set("n", "<leader>cl", ":clist<CR>", { desc = "List quickfix items" })
+vim.keymap.set("n", "<leader>cb", ":BuildQuickfix<CR>", { desc = "Load build errors from log" })
